ba6ab9254ea4febf315752c98c884cc82577abac
jfw2020 <jfw2020@ksu.edu>
2022-10-12 11:35:34 -0500

    Added eslint for client app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Button, Intent } from "@blueprintjs/core";
-import styled from "styled-components";
+import React from "react"
+import { Button, Intent } from "@blueprintjs/core"
+import styled from "styled-components"
 
 const StyledApp = styled.div`
 	
 `
 
 function App() {
-  return (
-	<StyledApp>
-		<Button intent={Intent.PRIMARY} text="Hello World!" />
-	</StyledApp>
-  );
+	return (
+		<StyledApp>
+			<Button intent={Intent.PRIMARY} text="Hello World!" />
+		</StyledApp>
+	)
 }
 
-export default App;
+export default App
