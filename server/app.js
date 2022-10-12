0c3baa4f776119dc5c08ae61de6e2aa8b91ced5d
jfw2020 <jfw2020@ksu.edu>
2022-10-12 11:11:07 -0500

    Installed CORS

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
@@ -12,6 +13,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
