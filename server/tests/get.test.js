cdf225ff2ba375413c165afc83d530e64a73af5d
Noah Sandford <nsandford64@ksu.edu>
2022-10-19 10:46:25 -0500

    Started testing with jest

---
+++ server/tests/get.test.js
@@ -0,0 +1,9 @@
+const server = require( "../server.js" )
+const supertest = require( "supertest" )
+const requestWithSupertest = supertest( server )
+
+describe("Get React App", () => {
+    it('GET / should return the React app', async () => {
+        const res = await 
+    })
+})
