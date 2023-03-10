a5917e2ba1706eab7f179460b44b503d1b95521b
jfw2020 <jfw2020@ksu.edu>
2023-03-10 13:27:24 -0600

    ShortAnswer component is now fully editable

--- client/src/components/multipleChoice.tsx
+++ client/src/components/multipleChoice.tsx
@@ -1,5 +1,5 @@
 // Copyright 2022 under MIT License
-import { InputGroup, Label, Radio, RadioGroup } from "@blueprintjs/core"
+import { Button, InputGroup, Label, Radio } from "@blueprintjs/core"
 import * as React from "react"
 import ReactMarkdown from "react-markdown"
 import styled from "styled-components"
@@ -12,6 +12,15 @@ import { examActions, selectQuestionById, selectSubmissionByUserIdAndQuestionId 
  */
 const StyledMultipleChoice = styled.div`
 	padding: 10px;
+`
+
+/**
+ * Style for the container while adding an answer
+ */
+const StyledAnswerInputContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 `
 
 /**
@@ -32,6 +41,13 @@ export const MultipleChoice = React.memo( ( props: ComponentProps ) => {
 	const submission = useAppSelector( state => selectSubmissionByUserIdAndQuestionId( state, props.questionId, props.canvasUserId ) )
 
 	/**
+	 * State
+	 */
+	const [ addingAnswer, setAddingAnswer ] = React.useState( false )
+	const [ editingAnswerIndex, setEditingAnswerIndex ] = React.useState( -1 )
+	const [ answer, setAnswer ] = React.useState( "" )
+
+	/**
 	 * Callbacks
 	 */
 	/*
@@ -39,19 +55,48 @@ export const MultipleChoice = React.memo( ( props: ComponentProps ) => {
 	calls the updateResponse delegate to update the state in the App
 	to reflect their choice.
 	*/
-	const handleChange = React.useCallback( ( e: React.FormEvent<HTMLInputElement> ) => {
-		// Create a new response object
-		const value = ( e.target as HTMLInputElement ).value
-
+	const handleChange = React.useCallback( ( value: number ) => {
+		// Create a new Submission object
 		const newSubmission: Submission = {
-			value: parseInt( value ),
+			value,
 			questionId: props.questionId
 		}
 
 		// Update the response in the store
 		dispatch( examActions.updateSubmission( newSubmission ) )
 	}, [] )
-	
+
+	// Called when an answer is added - updates the question in the store
+	const handleAddAnswer = React.useCallback( () => {
+		props.editQuestion( {
+			...question,
+			answers: [ ...question.answers, answer ]
+		} )
+
+		setAddingAnswer( false )
+	}, [ question, answer ] )
+
+	const handleRemoveAnswer = React.useCallback( ( index: number ) => {
+		props.editQuestion( {
+			...question,
+			answers: question?.answers.filter( ( _, answerIndex ) => answerIndex !== index )
+		} )
+
+		setEditingAnswerIndex( -1 )
+	}, [ question ] )
+
+	/**
+	 * Effects
+	 */
+	React.useEffect( () => {
+		setAddingAnswer( false )
+		setEditingAnswerIndex( -1 )
+	}, [ props.editable ] )
+
+	React.useEffect( () => {
+		setAnswer( "" )
+	}, [ addingAnswer ] )
+
 	/**
 	 * Render
 	 */
@@ -79,19 +124,85 @@ export const MultipleChoice = React.memo( ( props: ComponentProps ) => {
 					)}
 				</>				
 			)}
-			<RadioGroup
-				disabled={props.disabled}
-				onChange={handleChange}
-				selectedValue={submission?.value}
-			>
-				{question?.answers.map( ( choice, index ) => (
-					<Radio 
-						key={index} 
-						label={choice} 
-						value={index} 
+			{question?.answers.map( ( choice, index ) => (
+				<StyledAnswerInputContainer key={index} style={{ marginBottom: props.editable ? "10px" : undefined }}>
+					<Radio
+						key={index}
+						disabled={props.disabled}
+						value={index}
+						label={editingAnswerIndex === index ? undefined : choice}
+						checked={index === submission?.value}
+						onChange={() => handleChange( index )}
+						style={{ marginBottom: props.editable ? 0 : undefined }}
 					/>
-				) )}
-			</RadioGroup>
+					{editingAnswerIndex === index && (
+						<>
+							<InputGroup 
+								fill
+								value={choice}
+								onChange={e => props.editQuestion( {
+									...question,
+									answers: question?.answers.map( ( answer, answerIndex ) => {
+										if ( answerIndex === index ) {
+											return e.target.value
+										}
+
+										return answer
+									} )
+								} )}
+							/>
+							<Button 
+								icon="tick" 
+								style={{ marginLeft: 10 }} 
+								intent="success"
+								onClick={() => setEditingAnswerIndex( -1 )}
+							/>
+						</>
+					)}
+					{props.editable && editingAnswerIndex === -1 && (
+						<>
+							<Button 
+								icon="edit"
+								onClick={() => setEditingAnswerIndex( index )}
+								style={{ marginLeft: 10 }}
+							/>
+							<Button 
+								icon="cross" 
+								style={{ marginLeft: 10 }} 
+								intent="danger"
+								onClick={() => handleRemoveAnswer( index )}
+							/>
+						</>
+					)}
+				</StyledAnswerInputContainer>
+			) )}
+			{props.editable && !addingAnswer && question?.answers.length < 4 && (
+				<Button 
+					icon="plus"
+					onClick={() => setAddingAnswer( true )}			
+				/>
+			)}
+			{props.editable && addingAnswer && (
+				<StyledAnswerInputContainer>
+					<InputGroup 
+						fill
+						value={answer}
+						onChange={e => setAnswer( e.target.value )}
+					/>
+					<Button 
+						icon="cross"
+						intent="danger"
+						style={{ marginLeft: 10 }}
+						onClick={() => setAddingAnswer( false )}
+					/>
+					<Button 
+						icon="tick"
+						intent="success"
+						style={{ marginLeft: 10 }}
+						onClick={handleAddAnswer}
+					/>
+				</StyledAnswerInputContainer>
+			)}
 		</StyledMultipleChoice>
 	)
 } )
