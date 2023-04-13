d670ebceb6e08a0bb32d56476c6ed8ccda6679b6
Noah Sandford <nsandford64@ksu.edu>
2023-04-13 11:03:12 -0500

    Added delete query to new migration

--- server/migrations/20230413145122_add_unique_constraint.js
+++ server/migrations/20230413145122_add_unique_constraint.js
@@ -2,7 +2,15 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function( knex ) {
+exports.up = async function( knex ) {
+	
+	/** 
+	 * WARNING: this will delete all duplicate submission values from the student_responses table
+	 * This will keep the latest submission for each question for each student and delete all of the other submissions 
+	 * from that student for that question
+	 * */ 
+	await knex.raw( "delete from student_responses where (question_id, id) not in (select DISTINCT ON(question_id) question_id, id from student_responses order by question_id, id DESC);" )
+
 	return knex.schema
 		.alterTable( "student_responses", table => {
 			table.unique( [ "question_id", "user_id" ] )
