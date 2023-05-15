ab1301620285f87386f3a68ea54f1ec9b186ebbf
Nathan Bean <nhbean@ksu.edu>
2023-05-15 10:08:37 -0500

    Updated JSON size limit

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.set( "view engine", "jade" )
 
 app.use( cors() )
 app.use( logger( "dev" ) )
-app.use( express.json() )
+app.use( express.json({limit: "200kb"}) )
 app.use( express.urlencoded( { extended: false } ) )
 app.use( bodyParser.urlencoded( {extended: false} ) )
 app.use( cookieParser() )
